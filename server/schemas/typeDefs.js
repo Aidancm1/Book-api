36799f3d814d8d7b9e4856a1552056495a9f0d7f
Aidancm1 <128535723+Aidancm1@users.noreply.github.com>
2023-09-29 10:38:15 -0400

    typedefs start, need to double check

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,48 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs =
+const typeDefs = gql`
+# type User {
+#     _id: ID!
+#     name: String!
+# }
+
+type Query {
+    me: User
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(name: String!, email: String!, password: String!): Auth
+}
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+}
+input BookInput {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+`;
+
+
+module.exports = typeDefs;
