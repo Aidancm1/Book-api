6b64560366b9307761f6447241fc005de141466e
Aidancm1 <128535723+Aidancm1@users.noreply.github.com>
2023-10-03 17:53:38 -0400

    get me query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -3,5 +3,16 @@ import { gql } from '@apollo/client';
 // Create a GET_ME query 
 export const GET_ME = gql`
     query me {
-        
-    }
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }`
