413f79d004322f424466d7b0fa3d3ed0cc99f237
Aidancm1 <128535723+Aidancm1@users.noreply.github.com>
2023-10-03 17:51:45 -0400

    Save book and remove book mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,4 +23,23 @@ mutation addUser($email: String!, $password: String!, $username: String!) {
     }
 }`
 // Create a save_book,
+export const SAVE_BOOK = gql`
+mutation saveBook($bookId: String!, $authors: [String], $description: String!, $title: String!, $image: String!, $link: String!)`
+saveBook(bookId: $bookId, authors: $authors, description: $description, title: $title, image: $image, link: $link)
 // Create a remove_book
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+}`
