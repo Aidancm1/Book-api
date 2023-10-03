4b05380fa91743cdaffef328c366a5651af32a60
Aidancm1 <128535723+Aidancm1@users.noreply.github.com>
2023-10-03 17:46:08 -0400

    LOGIN user mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,16 @@
 import { gql } from '@apollo/client';
 
 // Crete a login_user,
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!, $username: String!) {
+        login(email: $email, password: $password, username: $username) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    })`
 // Create a Add_user,
 // Create a save_book,
 // Create a remove_book
